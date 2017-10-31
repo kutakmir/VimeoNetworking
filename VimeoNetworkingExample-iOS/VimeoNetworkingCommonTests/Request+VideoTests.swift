40dc6a3150615841be6fe84ae279e85c2096fc7e
jasonhawkins <hellojason@me.com>
2017-10-31 14:03:17 -0400

    Implement some stubbed Request+VideoTests

--- VimeoNetworkingExample-iOS/VimeoNetworkingCommonTests/Request+VideoTests.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingCommonTests/Request+VideoTests.swift
@@ -9,28 +9,31 @@
 import XCTest
 @testable import VimeoNetworking
 
-class Request_VideoTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class Request_VideoTests: XCTestCase
+{
+    func test_VideoRequest_ForVideoURI_ReturnsRequestWithCorrectURI()
+    {
+        let videoURI = "vimeo.com/123456789"
+        let videoRequest = Request<VIMVideo>.videoRequest(forVideoURI: videoURI)
+        XCTAssertTrue(videoURI == videoRequest.path, "The given URI does not match the path returned by the video request.")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func test_VideoRequest_PasswordProtectedVideoRequest_ReturnsRequestWithPasswordParameter()
+    {
+        let videoURI = "vimeo.com/123456789"
+        let password = "sekrets"
+        let passwordProtectedVideoRequest = Request<VIMVideo>.passwordProtectedVideoRequest(forVideoURI: videoURI, password: password)
+        let parameters = passwordProtectedVideoRequest.parameters as! [String: String]
+        XCTAssertTrue(password == parameters["password"], "The given password does not match the password provided as a paramter in the return request.")
     }
     
-    func test_VideoRequest_ForVideoURI_ReturnsRequestWithCorrectURI() {
-        
-    }
-    
-    func test_VideoRequest_PasswordProtectedVideoRequest_ReturnsRequestWithPasswordParameter() {
-        
-    }
-    
-    func test_VideoRequest_VODVideoRequest_ReturnsRequestWithVideoOverrideParameterSetToTrue() {
-        
+    func test_VideoRequest_VODVideoRequest_ReturnsRequestWithVideoOverrideParameterSetToTrue()
+    {
+        let vodVideoURI = "vimeo.com/1234556789"
+        let vodVideoRequest = Request<VIMVODItem>.vodVideoRequest(forVODVideoURI: vodVideoURI)
+        let parameters = vodVideoRequest.parameters as! [String: String]
+        XCTAssertTrue(vodVideoURI == vodVideoRequest.path)
+        XCTAssertTrue(parameters["_video_override"] == "true")
     }
     
     func test_VideoRequest_SelectedUsersRequest_ReturnsRequestWithPerPageKeySetTo100() {
